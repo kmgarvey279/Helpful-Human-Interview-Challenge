ad341a83db7d877b4e06381afaa9c07b4f670fef
Kevin Garvey <kmgarvey279@gmail.com>
2019-08-07 16:44:37 -0700

    Updates

---
+++ precache-manifest.373597b4c3b2e0ff376c378c76e86ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bcdf506c16ff0d134cc0d6235c2418",
+    "url": "/Helpful-Human-Interview-Challenge/index.html"
+  },
+  {
+    "revision": "3d87f5eaacb16194010c",
+    "url": "/Helpful-Human-Interview-Challenge/static/css/main.26951b97.chunk.css"
+  },
+  {
+    "revision": "ebe22e0791352a94b0b3",
+    "url": "/Helpful-Human-Interview-Challenge/static/js/2.5fca4cbf.chunk.js"
+  },
+  {
+    "revision": "3d87f5eaacb16194010c",
+    "url": "/Helpful-Human-Interview-Challenge/static/js/main.0ec13227.chunk.js"
+  },
+  {
+    "revision": "c303d8043b58bd16f498",
+    "url": "/Helpful-Human-Interview-Challenge/static/js/runtime~main.a74d9639.js"
+  },
+  {
+    "revision": "629f0661643c5026c42d228e37026f08",
+    "url": "/Helpful-Human-Interview-Challenge/static/media/logo-symbol.629f0661.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7925623ec44fe2889b302b74f049f897.js"
+  "/Helpful-Human-Interview-Challenge/precache-manifest.373597b4c3b2e0ff376c378c76e86ab8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Helpful-Human-Interview-Challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
